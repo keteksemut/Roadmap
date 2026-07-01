476dcf46abd03c2ee6f0898173ec6076b908f97d
Aditya Akbar <43461458+keteksemut@users.noreply.github.com>
2026-07-01 10:12:03 +0700

    Update greeting handling for invalid time of day
    Return null for invalid timeOfDay and handle it in createGreeting.

--- greetingBuilder.js
+++ greetingBuilder.js
@@ -8,14 +8,18 @@ function formatName(firstname, lastname) {
 function getGreeting(timeOfDay) {
   const norm = timeOfDay.toLowerCase();
   if (norm !== "morning" && norm !== "evening" && norm !== "afternoon") {
-    return "Greeting should be morning, evening, or afternoon";
+    return null;
   } else {
     return `Good ${timeOfDay}`;
   }
 }
 
 function createGreeting(firstname, lastname, timeOfDay) {
-  return getGreeting(timeOfDay) + ", " + formatName(firstname, lastname);
+  if (timeOfDay === null) {
+    return "Greeting should be morning, evening, or afternoon";
+  } else {
+    return getGreeting(timeOfDay) + ", " + formatName(firstname, lastname);
+  }
 }
 
 console.log(createGreeting("Herlambang", "johnson", "night"));
